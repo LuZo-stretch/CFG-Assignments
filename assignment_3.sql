25ee46e241403d10ec9b5517bfad2ab588679e4d
Lucia <lzgalarza@gmail.com>
2024-06-15 13:46:15 +0100

    Adding not null constraint to tables columns

--- assignment_3.sql
+++ assignment_3.sql
@@ -3,18 +3,18 @@ USE cinema_club;
 
 CREATE TABLE movies
 (movie_id INTEGER PRIMARY KEY,
-title VARCHAR(50),
-genre VARCHAR(15),
+title VARCHAR(50) NOT NULL,
+genre VARCHAR(15) NOT NULL,
 rating INTEGER);
 
 CREATE TABLE cinemas
 (cinema_id INTEGER PRIMARY KEY,
-cinema_name VARCHAR(20),
+cinema_name VARCHAR(20) NOT NULL,
 area VARCHAR(20),
 plushness INTEGER);
 
 CREATE TABLE club_members
 (member_id INTEGER PRIMARY KEY,
-member_name VARCHAR(20),
-member_email VARCHAR(20),
+member_name VARCHAR(20) NOT NULL,
+member_email VARCHAR(20) NOT NULL,
 area VARCHAR(20));
