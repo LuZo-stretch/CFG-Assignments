e89565d1a4dba7bf87725c62620d29388331bb07
Lucia <lzgalarza@gmail.com>
2024-06-16 15:21:06 +0100

    Added values for table cinemas

--- assignment_3.sql
+++ assignment_3.sql
@@ -18,3 +18,28 @@ CREATE TABLE club_members
 member_name VARCHAR(20) NOT NULL,
 member_email VARCHAR(20) NOT NULL,
 area VARCHAR(20));
+
+CREATE TABLE shows
+(cinema_id INTEGER,
+movie_id INTEGER);
+ALTER TABLE shows
+ADD CONSTRAINT fk_cinema_id
+FOREIGN KEY (cinema_id)
+REFERENCES cinemas (cinema_id);
+
+ALTER TABLE shows
+ADD CONSTRAINT fk_movie_id
+FOREIGN KEY (movie_id)
+REFERENCES movies (movie_id);
+
+INSERT INTO cinemas
+(cinema_id, cinema_name, area, plushness)
+VALUES
+(1, 'Ritzy Brixton', 'Brixton', 3),
+(2, 'Picture House WN', 'West Norwood', 4),
+(3, 'Prince Charles', 'Soho', 3),
+(4, 'Curzon Soho', 'Soho', 4),
+(5, 'Fanzy cinema', 'Bethnal Green', 5),
+(6, 'Creepy cinema', 'Greenwich', 2),
+(7, 'Falling apart cinema', 'Greenwich', 1),
+(8, 'Dating Spot', 'Bethnal Green', 5);
