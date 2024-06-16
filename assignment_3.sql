e96344ec78261bc8fdb5a69908819cf0ff5beecf
Lucia <lzgalarza@gmail.com>
2024-06-16 23:01:55 +0100

    Added comments to explain my queries

--- assignment_3.sql
+++ assignment_3.sql
@@ -19,12 +19,12 @@ member_name VARCHAR(20) NOT NULL,
 member_email VARCHAR(20) NOT NULL,
 area VARCHAR(20));
 
-CREATE TABLE shows
+CREATE TABLE shows --- This table will let me check which cinema is playing which movie at what time
 (cinema_id INTEGER,
 movie_id INTEGER,
 show_time TIME);
 ALTER TABLE shows
-ADD CONSTRAINT fk_cinema_id
+ADD CONSTRAINT fk_cinema_id --- Added FOREIGN KEYS to link to the relevant tables
 FOREIGN KEY (cinema_id)
 REFERENCES cinemas (cinema_id);
 
@@ -33,7 +33,7 @@ ADD CONSTRAINT fk_movie_id
 FOREIGN KEY (movie_id)
 REFERENCES movies (movie_id);
 
-INSERT INTO cinemas
+INSERT INTO cinemas --- Inserting cinemas values for each column
 (cinema_id, cinema_name, area, plushness)
 VALUES
 (1, 'Ritzy Brixton', 'Brixton', 3),
@@ -45,7 +45,7 @@ VALUES
 (7, 'Falling apart cinema', 'Greenwich', 1),
 (8, 'Dating Spot', 'Bethnal Green', 5);
 
-INSERT INTO club_members
+INSERT INTO club_members --- Inserting club members values
 (member_id, member_name, member_email, area)
 VALUES
 (1, 'Lucia Galarza', 'lucia@gmailcom', 'Brixton'),
@@ -57,7 +57,7 @@ VALUES
 (7, 'Fulano Tal', 'fulanito@gmail.com', 'Bethnal Green'),
 (8, 'Mengano Cual', 'menganito@gmailcom', 'Greenwich');
 
-INSERT INTO movies
+INSERT INTO movies --- Inserting movies values
 (movie_id, title, genre, rating)
 VALUES
 (1, 'Furiosa', 'action', 3),
@@ -69,7 +69,7 @@ VALUES
 (7, 'Scream', 'horror', 3),
 (8, 'Hard Miles', 'drama', 1);
 
-INSERT INTO shows
+INSERT INTO shows --- Adding the show times per movie and cinema
 (cinema_id, movie_id, show_time)
 VALUES
 (1, 1, '18:30:00'),
@@ -89,41 +89,41 @@ VALUES
 (8, 7, '21:30:00'),
 (8, 6, '22:00:00');
 
-SELECT * FROM shows;
+-- SELECT * FROM shows; --- Just checking all was showing correctly
 
-ALTER TABLE club_members
+ALTER TABLE club_members --- All emails must be unique to avoid members signing up other friends with just one email. 
 ADD CONSTRAINT unique_member_email UNIQUE (member_email);
 
-INSERT INTO club_members
+INSERT INTO club_members --- A new member joins
 (member_id, member_name, member_email)
 VALUES
 (9, 'Paco Escobar', 'paquito@gmail.com');
 
 SELECT * FROM club_members;
 
-SELECT cinema_id, cinema_name FROM cinemas
+SELECT cinema_id, cinema_name FROM cinemas --- I want to see all the cinemas in the Brixton area
 WHERE area = 'Brixton';
 
-SELECT member_id, member_name, member_email FROM club_members
+SELECT member_id, member_name, member_email FROM club_members --- I want to see what other members are in the Brixton area to organize a meet-up (the beginning of the drama)
 WHERE area = 'Brixton';
 
-SELECT movie_id, title, rating FROM movies --- AGGREGATE FUNCTION
+SELECT movie_id, title, rating FROM movies --- I only want to see movies that are rated above 3. 
 WHERE rating > 3 
 ORDER BY rating DESC;
 
-SELECT movie_id, title, rating FROM movies
+SELECT movie_id, title, rating FROM movies --- I want to see a scifi movie, what is playing now?
 WHERE genre = 'scifi';
 
-DELETE FROM club_members WHERE member_id = 9;
+DELETE FROM club_members WHERE member_id = 9; --- I just saw that Paco Escobar didn't specify an area, it will be difficult to know what events suit him and after emailing him several times to ask, he never replied. We are deleting him as a member. 
 SELECT * FROM club_members;
 
-SELECT area, AVG(plushness) AS average_plushness
+SELECT area, AVG(plushness) AS average_plushness --- I want to see the average plushness level of cinemas in each area
 FROM cinemas
 GROUP BY area
 HAVING AVG(plushness) > 2
-ORDER BY average_plushness DESC; --- AGGREGATE FUNCTION
+ORDER BY average_plushness DESC; 
 
-SELECT cm.member_name, cm.member_email --- JOIN TO GROUP ALL CLUB MEMBERS THAT ARE IN THE BRIXTON AREA WITH THE RELEVANT CINEMA
+SELECT cm.member_name, cm.member_email --- JOIN TO SHOW ALL CLUB MEMBERS THAT ARE IN THE BRIXTON AREA, THEIR NAME, EMAIL AND THE RELEVANT CINEMA
 FROM club_members cm
 INNER JOIN
 cinemas c
@@ -135,7 +135,7 @@ FROM cinemas c
 LEFT JOIN shows s ON c.cinema_id = s.cinema_id
 ORDER BY  c.cinema_name;
 
-SELECT cm.member_name, cm.member_email, c.cinema_name, m.title AS movie_title, s.show_time --- JOIN TO SHOW ALL THE MEMBERS THE CINEMAS, MOVIES AND SHOW TIMES IN THE SAME AREA
+SELECT cm.member_name, cm.member_email, c.cinema_name, m.title AS movie_title, s.show_time --- JOIN TO SHOW ALL THE MEMBERS, THE CINEMAS, MOVIES, AND SHOW TIMES IN THE SAME AREA
 FROM club_members cm
 INNER JOIN cinemas c ON cm.area = c.area
 INNER JOIN shows s ON c.cinema_id = s.cinema_id
@@ -152,8 +152,8 @@ INNER JOIN movies ON shows.movie_id = movies.movie_id;
 SELECT CONCAT(cinema_name, ', ', area) AS cinema_address
 FROM cinemas;
 
-DELIMITER //
-CREATE PROCEDURE GetMoviesByCinemaBis(cinema_id_param INTEGER)
+DELIMITER //  --- CREATE PROCEDURE TO SHOW ALL MOVIES BY CINEMA WITH THE MOVIE INFO AND THE CINEMA THEY ARE PLAYING AT. 
+CREATE PROCEDURE GetMoviesByCinema(cinema_id_param INTEGER)
 BEGIN
 SELECT m.title, m.genre, m.rating, c.cinema_name
 FROM movies m
@@ -164,4 +164,4 @@ END //
 
 DELIMITER ;
 
-CALL GetMoviesByCinemaBis(1);
+CALL GetMoviesByCinema(1); --- CALLING THE PROCEDURE FOR THE CINEMA WITH ID 1 
